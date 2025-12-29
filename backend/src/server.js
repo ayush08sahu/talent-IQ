be075bdfb0ad6c38619e43c50ee051d08497716e
ayush08sahu <www.sahuayush54321@gmail.com>
2025-12-29 21:39:06 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { ENV } from './lib/env.js';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({msg: "Success from API"})
+})
+
+app.listen(ENV.PORT, () => 
+    console.log("Server running on port :", ENV.PORT
+))
