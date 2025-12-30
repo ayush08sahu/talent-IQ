92f8ee39fe3749fe74db821e4af55c751f57f095
ayush08sahu <www.sahuayush54321@gmail.com>
2025-12-30 21:57:59 +0530

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,26 @@
-import express from 'express';
-import { ENV } from './lib/env.js';
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({msg: "Success from API"})
-})
+const __dirname = path.resolve();
 
-app.listen(ENV.PORT, () => 
-    console.log("Server running on port :", ENV.PORT
-))
+app.get("/success", (req, res) => {
+  res.status(200).json({ msg: "Success from API" });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "API is healthy" });
+});
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => console.log("Server running on port :", ENV.PORT));
